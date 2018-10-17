63e8183df91b878d0917efa330ad523d92883c7b
Raymund Martinez <zhaqenl@gmail.com>
2018-10-17 21:35:01 +0800

    Add staging server to FT runner

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-import time
+import time, os
 
 MAX_WAIT = 10
 
@@ -10,6 +10,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'https://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
